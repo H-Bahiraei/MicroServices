17578ecb1fb6182195b8d35fadbfe3f5277e736a
h-bahiraei <Mehrad_x7>
2022-10-31 08:51:37 +0330

    f4: adding Eureka Server
    Signed-off-by: h-bahiraei <Mehrad_x7>

---
+++ Eureka_server/src/test/java/org/gfn/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gfn.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
